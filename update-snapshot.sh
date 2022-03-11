e855b735bbe5eee6e57d07a070633725d53db075
Dmitry K <okdas@pm.me>
2022-03-11 15:49:48 -0800

    par 64 -> 32

--- update-snapshot.sh
+++ update-snapshot.sh
@@ -3,20 +3,6 @@ set -e
 
 export $(grep -v '^#' .env | xargs)
 
-# POCKET_NODE_HTTP_URL="${POCKET_NODE_HTTP_URL:-http://localhost:8080/v1}"
-POCKET_NODE_DOCKER_CONTAINER_NAME="${POCKET_NODE_DOCKER_CONTAINER_NAME:-"peer1"}"
-POCKET_NODE_DATA_DIR="${POCKET_NODE_DATA_DIR:-"/data/peer1"}"
-POCKET_SNAPSHOT_PATH="${POCKET_SNAPSHOT_PATH:-"/data/pocket-data-snapshot.tar"}"
-POCKET_SNAPSHOT_HASHES_PATH="${POCKET_SNAPSHOT_HASHES_PATH:-"/data/pocket-data-snapshot-hashes.txt"}"
-UPLINK_WRITE="${UPLINK_WRITE:-""}"
-API_GATEWAY_ID="${API_GATEWAY_ID:-"nwu2m3bz8h"}"
-API_GATEWAY_LATEST_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar`].id | [0]')
-API_GATEWAY_LATEST_MD5_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar/md5`].id | [0]')
-API_GATEWAY_LATEST_SHA1_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar/sha1`].id | [0]')
-API_GATEWAY_LATEST_GZ_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz`].id | [0]')
-API_GATEWAY_LATEST_GZ_MD5_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz/md5`].id | [0]')
-API_GATEWAY_LATEST_GZ_SHA1_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz/sha1`].id | [0]')
-
 check_cli_tools_installed() {
     for p in pigz uplink rhash aws; do
         hash "$p" &>/dev/null && echo "$p is installed" ||
@@ -27,10 +13,27 @@ check_cli_tools_installed() {
 # get_node_version() {
 # }
 
-datetime_tar_name=$(date +"pocket-network-data-%H%d.tar")
+# POCKET_NODE_HTTP_URL="${POCKET_NODE_HTTP_URL:-http://localhost:8080/v1}"
+POCKET_NODE_DOCKER_CONTAINER_NAME="${POCKET_NODE_DOCKER_CONTAINER_NAME:-"peer1"}"
+POCKET_NODE_DATA_DIR="${POCKET_NODE_DATA_DIR:-"/data/peer1"}"
+POCKET_SNAPSHOT_PATH="${POCKET_SNAPSHOT_PATH:-"/data/pocket-data-snapshot.tar"}"
+POCKET_SNAPSHOT_HASHES_PATH="${POCKET_SNAPSHOT_HASHES_PATH:-"/data/pocket-data-snapshot-hashes.txt"}"
+UPLINK_WRITE="${UPLINK_WRITE:-""}"
+API_GATEWAY_ID="${API_GATEWAY_ID:-"nwu2m3bz8h"}"
+
 
 # Check dependencies
 check_cli_tools_installed
+
+API_GATEWAY_LATEST_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar`].id | [0]')
+API_GATEWAY_LATEST_MD5_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar/md5`].id | [0]')
+API_GATEWAY_LATEST_SHA1_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar/sha1`].id | [0]')
+API_GATEWAY_LATEST_GZ_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz`].id | [0]')
+API_GATEWAY_LATEST_GZ_MD5_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz/md5`].id | [0]')
+API_GATEWAY_LATEST_GZ_SHA1_RESOURCE_ID=$(aws apigateway get-resources --rest-api-id "$API_GATEWAY_ID" --output text --query 'items[?path==`/latest.tar.gz/sha1`].id | [0]')
+
+datetime_tar_name=$(date +"pocket-network-data-%H%d.tar")
+
 
 # Delete old archives if they exist
 sudo rm -f "$POCKET_SNAPSHOT_PATH" "$POCKET_SNAPSHOT_PATH".gz
@@ -63,9 +66,9 @@ rhash -MH "$POCKET_SNAPSHOT_PATH"* | sudo tee "$POCKET_SNAPSHOT_HASHES_PATH"
 # upload tar, tar.gz
 
 echo "Uploading $POCKET_SNAPSHOT_PATH to sj://pocket-public-blockchains/$datetime_tar_name"
-uplink cp --access "$UPLINK_WRITE" "$POCKET_SNAPSHOT_PATH" "sj://pocket-public-blockchains/$datetime_tar_name" --parallelism 64
+uplink cp --access "$UPLINK_WRITE" "$POCKET_SNAPSHOT_PATH" "sj://pocket-public-blockchains/$datetime_tar_name" --parallelism 32
 echo "Uploading $POCKET_SNAPSHOT_PATH to sj://pocket-public-blockchains/$datetime_tar_name.gz"
-uplink cp --access "$UPLINK_WRITE" "${POCKET_SNAPSHOT_PATH}.gz" "sj://pocket-public-blockchains/$datetime_tar_name.gz" --parallelism 64
+uplink cp --access "$UPLINK_WRITE" "${POCKET_SNAPSHOT_PATH}.gz" "sj://pocket-public-blockchains/$datetime_tar_name.gz" --parallelism 32
 
 # get sharable links
 tar_link=$(uplink share --url --access "$UPLINK_WRITE" "sj://pocket-public-blockchains/$datetime_tar_name" |  grep "URL  " | awk  '{print $3}')
